0d7061d7517782fc1d62a2db6a973293a4f84745
Rasel <rasel.ahammed@wcgclinical.com>
2023-05-20 14:40:29 +0600

    SpaceX service added for getting upcoming launches

---
+++ src/services/SpaceXService.js
@@ -0,0 +1,10 @@
+const SpaceXApiBaseUrl = process.env.REACT_APP_SPACEX_API; 
+
+export async function getUpcomingLaunches(pageNumber = 1) {
+    try {
+        const response = await fetch(`${SpaceXApiBaseUrl}/spacex/launches/upcoming?pageNumber=${pageNumber}`);
+        return await response.json();
+    } catch (error) {
+        return [];
+    }
+}
